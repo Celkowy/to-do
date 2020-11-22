ba59bb07591fc79f187e0ffb9e435f40b576b2e7
tucelek@gmail.com <tucelek@gmail.com>
2020-11-22 19:35:58 +0100

    not working

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const notCompletedTable = [];
-const CompletedTable = [];
+const notCompletedTodos = [];
+const completedTodos = [];
 const input = document.querySelector('form input')
 const form = document.querySelector('form')
-const ul = document.querySelector(".ul-not-completed")
+const ulNotCompleted = document.querySelector(".ul-not-completed")
+const ulCompleted = document.querySelector(".ul-completed")
 const counter = document.querySelector('.counter')
 
 
@@ -10,54 +11,98 @@ const addTodo = (e) => {
   e.preventDefault();
   if(input.value === "") return
   const newTodo = document.createElement("li")
-  newTodo.innerHTML = `<div class="left"><span class="index"></span><input class="checkbox" type="checkbox"> ${input.value}</div><div class="right"><i class="far fa-check-square"></i><i class="fas fa-edit"></i><i class="fas fa-trash remove"></i></div>` 
-  ul.appendChild(newTodo)
-  notCompletedTable.push(newTodo)
-
-
-
-  const remove = newTodo.querySelector('.remove');
+  newTodo.innerHTML = `<div class="left"><span class="index"></span>${input.value}</div><div class="right"><i class="fas fa-edit"></i><i class="far fa-check-square"></i></div>`;
+  ulNotCompleted.appendChild(newTodo)
+  notCompletedTodos.push(newTodo)
+  const remove = newTodo.querySelector('.fa-check-square');
   remove.addEventListener('click', removeTodo)
   updateOrder()
   updateTable()
-
+  updateCompletedLiIndexes()
+  deleteFromCompletedUl()
   input.value = ""
-  console.log(notCompletedTable);
 }
 
 const removeTodo = (e) => {
-
+  e.target.parentNode.previousSibling.childNodes[0].classList.add('hide')
+  e.target.parentNode.childNodes[0].classList.add('hide')
+  e.target.parentNode.childNodes[1].classList.add('hide')
+  e.target.parentNode.previousSibling.classList.add('line-through')
+  e.target.className = "fas fa-trash delete"
+  e.target.parentNode.parentNode.classList.add('skip')
   const del = e.target.parentNode.parentNode;
   del.remove()
-  notCompletedTable.splice(del.dataset.key,1)
-  updateTable()
+  notCompletedTodos.splice(del.dataset.key,1)
+  completedTodos.push(e.target.parentNode.parentNode)
   updateOrder()
-  
-  console.log(notCompletedTable);
+  updateTable()
+  updateCompletedTable()
+  deleteFromCompletedUl()
+  updateCompletedLiIndexes()
+
 }
 
 const updateTable = () => {
-
-  ul.textContent = ""
-  notCompletedTable.forEach((element, index) =>{
+  ulNotCompleted.textContent = ""
+  notCompletedTodos.forEach((element, index) =>{
     element.dataset.key = index;
-    ul.appendChild(element)
+    ulNotCompleted.appendChild(element)
   })
-  counter.textContent = notCompletedTable.length
+  counter.textContent = notCompletedTodos.length
 }
 
 const updateOrder = () => {
-
-  let allLi = [...document.querySelectorAll('.index')]
-  allLi.forEach((element, index) => {
+  let allnotCompletedLi = [...document.querySelectorAll('.index')]
+  allnotCompletedLi.forEach((element, index) => {
     element.textContent = index + 1
   })
 }
 
 
+const updateCompletedTable = () => {
+  completedTodos.forEach(element => {
+    ulCompleted.appendChild(element)
+  })
+}
+
+const deleteFromCompletedUl = () => {
+  let listOfDeleteElements = [...document.querySelectorAll('.delete')]
+  listOfDeleteElements.forEach((element, index) => {
+   element.dataset.key = index;
+  })
+
+  //  ulCompleted.textContent = ""
+  //   completedTodos.forEach((element, index) => {
+  //   element.dataset.key = index;
+  //   ulCompleted.appendChild(element)
+  // })
+
+  // listOfDeleteElements.forEach(element => {
+  //   element.addEventListener('click', () => {
+  //     completedTodos.forEach(todo => {
+  //     updateCompletedLiIndexes()
+        
+  //       console.log(todo, element);
+  //       if(todo.dataset.key == element.dataset.key){
+  //         console.log(todo.dataset.key);
+  //         completedTodos.splice(todo.dataset.key, 1)
+  //         console.log(todo);
+  //         todo.remove()
+  //       }
+  //     })
+  //   })
+  // })
+
+}
 
 
+const updateCompletedLiIndexes = () => {
+  completedTodos.forEach((element, index) => {
+  element.dataset.key = index;
+  console.log(element);
+})
 
+}
 
 
 
